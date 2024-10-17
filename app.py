d46a9ee23d5731e4a56b8e6e568b858c1a74c6b4
campus3 S005 <s005@alpacoedu.onmicrosoft.com>
2024-10-17 10:44:47 +0900

    new

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 def main():
     arr = np.random.normal(1, 1, size=100)
     fig, ax = plt.subplots()
-    ax.hist(arr, bins=20)
+    ax.hist(arr, bins=20)# 업데이트
 
     st.pyplot(fig)
 
